a615046d581df27d4dc5334ffadeb1917e73a14b
Anas Maarouf <Thefatblock@gmail.com>
2023-03-17 14:06:45 +0100

    Initial project commit.

---
+++ Personal_Debts_Book.App/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Personal_Debts_Book.App.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace Personal_Debts_Book.App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ Personal_Debts_Book.App/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Personal_Debts_Book.App.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
